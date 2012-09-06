b4e4c9e7dfabb4d0db65cd28b02bb3c6db171d26
gregor <info@klangkontrast.net>
2012-09-06 03:35:18 +0200

    update form for letter of intent

---
+++ js/core.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: G. Jahn
+ * Date: 06.09.12
+ * Time: 01:01
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function(){
+
+    $('#openform').click(function(){
+
+        $('#decOfIntent').modal('show');
+
+    });
+
+});
